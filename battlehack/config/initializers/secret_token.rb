e4681f7695356fc5c1f216bd2e848f71be5b5ee2
Akshay Mehra <akshay.m.1711@gmail.com>
2015-07-18 14:39:22 -0400

    hobo framework up

---
+++ battlehack/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Battlehack::Application.config.secret_key_base = 'c2ca4fd767cedda506582e885b3310ebd0479087b117989a2a937740c79ae1b2b036312ac55c5475f9313bfae33edeedc861acc455e6ebb2b86f95df650d5043'
